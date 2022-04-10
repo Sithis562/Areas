e0d76e623d8517683119ef787e8147e1e1bb30a1
Sithis <geraltovna123@yandex.ru>
2022-04-10 15:49:30 +0300

    Добавьте файлы проекта.

---
+++ Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areas
+{
+    public static class Circle
+    {
+        public static double Area(double R)
+        {
+            return Math.PI * Math.Pow(R, 2);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areas
+{
+    public static class Triangle
+    {
+        public static double Area(double firstSide, double secondSide, double thirdSide)
+        {
+            double p, S;
+
+            p = (firstSide + secondSide + thirdSide) / 2;
+
+            return S = Math.Sqrt(p * (p - firstSide) * (p - secondSide) * (p - thirdSide));
+        }
+    }
+}
